c2a263fb761ba9549784b62a32165124d74d6186
hritikbhattacharya <hritik7042@gmail.com>
2024-03-02 23:07:44 +0530

    Server part updated

--- server.js
+++ server.js
@@ -9,18 +9,33 @@ app.use(express.json());
 app.use(express.static("build"));
 
 app.post("/api/saveData", (req, res) => {
-  fs.writeFile(
-    "UserResponse.json",
-    JSON.stringify(req.body, null, 2),
-    (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("An error occurred while writing to the file.");
-      } else {
+  fs.readFile("UserResponse.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .send("An error occurred while reading from the file.");
+    }
+
+    let existingData = JSON.parse(data || "[]");
+
+    existingData.push(req.body);
+
+    fs.writeFile(
+      "UserResponse.json",
+      JSON.stringify(existingData, null, 2),
+      (err) => {
+        if (err) {
+          console.error(err);
+          return res
+            .status(500)
+            .send("An error occurred while writing to the file.");
+        }
+
         res.status(200).send("Data successfully written to file.");
       }
-    }
-  );
+    );
+  });
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
